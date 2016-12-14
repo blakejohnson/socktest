3b523e3c53b11498069c9a7f4433406b27556051
Blake Johnson <blakejohnson04@gmail.com>
2016-12-13 22:28:16 -0500

    Add compilation note.

--- socktest.h
+++ socktest.h
@@ -1,5 +1,10 @@
 #include <stdint.h>
 
+// to compile:
+// gcc -c -fpic socktest.cpp
+// gcc -shared -o libsocket.dll socktest.o -lws2_32 (windows)
+// or
+// gcc -shared -o libsocket.dylib socktest.o (macOS)
 
 extern "C" {
     int32_t set_socket(int fd);
